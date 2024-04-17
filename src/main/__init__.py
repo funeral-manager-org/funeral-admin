aa2ae2577e6516a1fb8ff99ac8c1cad9c14910df
freelancing-solutions <mobiusndou@gmail.com>
2024-04-17 11:51:58 +0200

    disabled firewall

--- src/main/__init__.py
+++ src/main/__init__.py
@@ -68,7 +68,7 @@ def create_app(config):
     with app.app_context():
         from src.main.bootstrapping import bootstrapper
         bootstrapper()
-        firewall.init_app(app=app)
+        # firewall.init_app(app=app)
 
         _add_blue_prints(app)
         _add_filters(app)

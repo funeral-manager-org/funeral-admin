c34fc7478d54eabf94d6b25c75a5dfef27fc7992
freelancing-solutions <mobiusndou@gmail.com>
2024-04-17 11:38:55 +0200

    changed port

--- app.py
+++ app.py
@@ -5,4 +5,4 @@ app, chat_io = create_app(config=config_instance())
 
 if __name__ == '__main__':
     # chat_io.run(app, port=5000, debug=True)
-    app.run(debug=True)
+    app.run(debug=True, port=8000)

1b5caed5a032f93ceb46e6a9adc27ab37acf3543
mobius-crypt <mobiusndou@gmail.com>
2024-06-15 11:34:44 +0200

    code refactor

--- src/routes/covers.py
+++ src/routes/covers.py
@@ -1,18 +1,15 @@
 from flask import Blueprint, render_template, url_for, flash, redirect, request
 from pydantic import ValidationError
 
-from src.database.models.bank_accounts import BankAccount
-from src.database.models.contacts import Address, PostalAddress, Contacts
 from src.authentication import login_required
-from src.database.models.companies import Company, CompanyBranches, EmployeeDetails, CoverPlanDetails
+from src.database.models.companies import CoverPlanDetails
 from src.database.models.users import User
-from src.main import company_controller, user_controller, encryptor
-from src.utils import create_id
+from src.main import company_controller
 
 covers_route = Blueprint('covers', __name__)
 
 
-@covers_route.get('/admin/administrator/covers')
+@covers_route.get('/admin/administrator/plans')
 @login_required
 async def get_covers(user: User):
     """
@@ -26,7 +23,7 @@ async def get_covers(user: User):
     return render_template('admin/managers/covers.html', **context)
 
 
-@covers_route.post('/admin/administrator/covers/add-plan-cover')
+@covers_route.post('/admin/administrator/plans/add-plan-cover')
 @login_required
 async def add_plan_cover(user: User):
     """
@@ -34,7 +31,6 @@ async def add_plan_cover(user: User):
         :return:
     """
     try:
-
         plan_cover = CoverPlanDetails(**request.form)
         print(plan_cover)
         plan_cover.company_id = user.company_id
@@ -49,7 +45,7 @@ async def add_plan_cover(user: User):
     return redirect(url_for('covers.get_covers'))
 
 
-@covers_route.get('/admin/administrator/cover/<string:company_id>/<string:plan_number>')
+@covers_route.get('/admin/administrator/plan/<string:company_id>/<string:plan_number>')
 @login_required
 async def get_plan_cover(user: User, company_id: str, plan_number: str):
     """
@@ -61,11 +57,9 @@ async def get_plan_cover(user: User, company_id: str, plan_number: str):
     """
     if user.company_id != company_id:
         flash(message="You are not authorized to view the cover details", category="danger")
-
         return redirect('home.get_home')
 
     plan_cover = await company_controller.get_plan_cover(company_id=company_id, plan_number=plan_number)
     subscribed_clients = await company_controller.get_plan_subscribers(plan_number=plan_number)
     context = dict(user=user, plan_cover=plan_cover, subscribed_clients=subscribed_clients)
     return render_template('admin/managers/covers/view.html', **context)
-

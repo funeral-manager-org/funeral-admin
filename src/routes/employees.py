7934e50f2a496020c0c771c47a7c3f93a9fe7465
mobius-crypt <mobiusndou@gmail.com>
2024-07-30 22:06:54 +0200

    eliminated the bug where employee detail does not exists

--- src/routes/employees.py
+++ src/routes/employees.py
@@ -11,20 +11,26 @@ from src.main import company_controller, employee_controller
 employee_route = Blueprint('employees', __name__)
 employee_logger = init_logger('employee_route')
 
+
 async def add_data_employee(context, employee_detail):
+    if not employee_detail:
+        return
+
     if employee_detail.contact_id:
         contact_details = await company_controller.get_contact(contact_id=employee_detail.contact_id)
         context.update(contact_details=contact_details)
+
     if employee_detail.address_id:
         physical_address = await company_controller.get_address(address_id=employee_detail.address_id)
         context.update(physical_address=physical_address)
+
     if employee_detail.postal_id:
         postal_address = await company_controller.get_postal_address(postal_id=employee_detail.postal_id)
         context.update(postal_address=postal_address)
+
     if employee_detail.bank_account_id:
         bank_account = await company_controller.get_bank_account(bank_account_id=employee_detail.bank_account_id)
         context.update(bank_account=bank_account)
-
 
 
 @employee_route.get('/admin/employee-details')
@@ -40,13 +46,15 @@ async def get_employee_details(user: User):
         message: str = "you have no proper employee record please inform admin"
         flash(message=message, category="danger")
         return redirect(url_for('home.get_home'))
+    context = dict(user=user)
     employee_detail: EmployeeDetails = await employee_controller.get_employee_complete_details_uid(
         uid=user.uid)
-    context = dict(user=user, employee_detail=employee_detail)
-    # this adds postal addresses and others
-    await add_data_employee(context=context, employee_detail=employee_detail)
+    if employee_detail:
+        context.update(employee_detail=employee_detail)
+        # this adds postal addresses and others
+        await add_data_employee(context=context, employee_detail=employee_detail)
 
-    context = dict(user=user, employee_detail=employee_detail)
+        context = dict(user=user, employee_detail=employee_detail)
     return render_template('admin/managers/employees/view.html', **context)
     # return render_template('admin/employees/employee.html', **context)
 
@@ -92,7 +100,6 @@ async def get_employee_detail(user: User, employee_id: str):
     await add_data_employee(context, employee_detail)
 
     return render_template('admin/managers/employees/view.html', **context)
-
 
 
 @employee_route.get('/admin/employees/attendance-register')

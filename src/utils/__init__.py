350a1ccb2258d78df506371e7e584d5fcb505327
mothetho <mobiusndou@gmail.com>
2024-08-14 23:03:09 +0200

    Increased Policy Numbers, Employee Numbers and Claim Numbers to 12 characters each plan number is still 9 characters long

--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -153,19 +153,19 @@ def create_plan_number():
 
 
 def create_claim_number():
-    random_chars = ''.join(random.choices(string.ascii_uppercase + string.digits, k=9))
+    random_chars = ''.join(random.choices(string.ascii_uppercase + string.digits, k=12))
     random_chars_uppercase = random_chars.upper()
     return random_chars_uppercase
 
 
 def create_policy_number():
-    random_chars = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    random_chars = ''.join(random.choices(string.ascii_uppercase + string.digits, k=12))
     random_chars_uppercase = random_chars.upper()
     return random_chars_uppercase
 
 
 def create_employee_id():
-    random_chars = ''.join(random.choices(string.ascii_uppercase + string.digits, k=6))
+    random_chars = ''.join(random.choices(string.ascii_uppercase + string.digits, k=12))
     random_chars_uppercase = random_chars.upper()
     return random_chars_uppercase
 

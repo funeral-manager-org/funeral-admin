76f2dd049a90aeb18a9cb902fff21b2972dd2525
mobius-crypt <mobiusndou@gmail.com>
2025-08-25 18:03:09 +0200

    Update run_asgi.py

--- run_asgi.py
+++ run_asgi.py
@@ -2,6 +2,7 @@
 from src.main import create_app
 from src.config import config_instance
 
+# Create the async Flask app
 app, message_controller, notifications_controller, subscriptions_controller = create_app(
     config=config_instance()
 )
@@ -10,9 +11,9 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run(
-        "src.main:create_app()",  # if using factory, call it with quotes
+        app,                  # pass the actual ASGI app object
         host="0.0.0.0",
         port=8000,
-        reload=True,  # optional, for dev autoreload
+        reload=True,          # dev autoreload
         log_level="info",
     )
